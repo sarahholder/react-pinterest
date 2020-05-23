2d8c454d1e6040e51e230d34d6722a7ca347fbe8
Sarah Holder <sr.holder@yahoo.com>
2020-05-23 12:05:54 -0500

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>This is Pinterest Again!</h1>
+      <button className="btn btn-danger">Hello</button>
+      </div>
+    )
+  }
+}
+
+
+export default App;
